95a5d5944904a8e9613d61df0fe01f733b3db51b
Tomek Felisiak <tomekfel@googlemail.com>
2018-03-27 18:02:04 +0100

    still async

--- app.js
+++ app.js
@@ -6,36 +6,55 @@ request('https://www.ddlvalley.me/category/movies/', function(error, response, h
         var $ = cheerio.load(html);
         var parsedResults = [];
 
+
         $('div.cont.cl > div:nth-child(1) > div > div > a:nth-child(1)').each(function(i, element) {
-            var a = $(this).attr('href');
-            var b = $(this).next().attr('href');
-            var link = a.indexOf('imdb.com') < 0 ? b : a;
-            var c = $(this).parent().parent().parent().parent();
-            var title = c.parent().prev().text();
-            var meta = c.prev().children('span.fl').children('span.date').text();
-            var rating = 0;
 
-            // Our parsed meta data object
-            var metadata = {
-                link: link,
-                title: title,
-                meta: meta
-            };
+            let vm = $(this);
 
-            // request to IMDB
-            request(link, function(error, response, html) {
-                if (!error && response.statusCode == 200) {
-                    var $ = cheerio.load(html);
-                    $('div.ratingValue strong span').each(function(i, element) {
-                        rating = $(this).html();
-                        metadata.rating = rating;
-                    });
-                    // Push meta-data into parsedResults array
-                    parsedResults.push(metadata);
-                    // Log our finished parse results in the terminal
-                    console.log(parsedResults);
-                }
-            });
+            doThis(andThenThis);
+
+            function doThis(callback) {
+                console.log('do This');
+
+                var a = vm.attr('href');
+                var b = vm.next().attr('href');
+                var link = a.indexOf('imdb.com') < 0 ? b : a;
+                var c = vm.parent().parent().parent().parent();
+                var title = c.parent().prev().text();
+                var meta = c.prev().children('span.fl').children('span.date').text();
+                var rating = 0;
+
+                // Our parsed meta data object
+                var metadata = {
+                    link: link,
+                    title: title,
+                    meta: meta
+                };
+
+                //console.log(metadata);
+
+                callback(metadata);
+            }
+
+            function andThenThis(metadata) {
+                console.log('and Then This');
+                // request to IMDB
+                request(metadata.link, function(error, response, html) {
+                    if (!error && response.statusCode == 200) {
+                        var $ = cheerio.load(html);
+                        $('div.ratingValue strong span').each(function(i, element) {
+                            rating = $(this).html();
+                            metadata.rating = rating;
+                        });
+                        // Push meta-data into parsedResults array
+                        parsedResults.push(metadata);
+                        if (parsedResults.length == 10) {
+                            console.log(parsedResults);
+                        }
+                    }
+                });
+            }
         });
+
     }
 });
